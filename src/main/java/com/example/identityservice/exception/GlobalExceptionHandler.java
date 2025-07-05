5d8f0f6b9cfe0c39b20e4828c43eb49494232ab7
Nguyễn Hoàng Linh <dev.hoanglinh@gmail.com>
2025-07-05 11:57:33 +0700

    feat: valid with CUSTOM annotation part2 - show detail msg by replace errorCode

--- src/main/java/com/example/identityservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/identityservice/exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.example.identityservice.exception;
 
 import com.example.identityservice.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
     //    EXCEPTION bat cac loi con lai khong bat duoc
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
@@ -33,14 +42,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(value = AccessDeniedException.class)
-    // nem tu PreAuthorize
+        // nem tu PreAuthorize
     ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
         ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
 
-        return ResponseEntity.status(errorCode.getStatusCode()).body(ApiResponse.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build());
+        return ResponseEntity.status(errorCode.getStatusCode()).body(ApiResponse.builder().code(errorCode.getCode()).message(errorCode.getMessage()).build());
     }
 
     //    EXCEPTION bat loi Valid
@@ -49,15 +55,31 @@ public class GlobalExceptionHandler {
         String enumKey = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+
+        Map<String, Object> attributes = null;
+
+
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
+
         } catch (IllegalArgumentException e) {
         }
 
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ? mapAtrribute(errorCode.getMessage(), attributes) :  errorCode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAtrribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf( attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
